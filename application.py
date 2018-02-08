dd38f4e14b00ac6db4d3cb1cca9e29c051e7c95c
Suchitra Sompalli <suchitra.sompalli@gmail.com>
2018-02-08 14:07:46 -0600

    feat: Wrote out user created data to a file and reading it back for play.

--- application.py
+++ application.py
@@ -1,24 +1,36 @@
 """Main application code handling http requests."""
-from flask import Flask, render_template
+from __future__ import print_function
+from flask import Flask, render_template, request
 
+import sys
 
 app = Flask(__name__)
-
 
 APPLICATION_NAME = "MadLibs"
 
 
 @app.route('/')
-@app.route('/madlibs/')
+@app.route('/madlibs/', methods=['GET', 'POST'])
 def showMainPage():
     """Main page."""
-    return render_template('generate.html')
+    if request.method == 'POST':
+        print(request.form["madlib-text"], file=sys.stderr)
+        file = open("madlib.txt", "w")
+        file.write(request.form["madlib-text"])
+        file.close()
+        return render_template('success.html')
+    else:
+        return render_template('generate.html')
 
 
 @app.route('/madlibs/play/')
 def showPlayPage():
     """Play page."""
-    return render_template('play.html')
+    file = open("madlib.txt", "r")
+    madlibData = ''
+    for line in file:
+        madlibData += line
+    return render_template('play.html', madlibData=madlibData)
 
 
 if __name__ == '__main__':
